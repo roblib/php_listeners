5f5ba802c15fcabbc1b83f0089c46e54fcd8367b
CS482 <CS482@tlrmcknz.com>
2013-04-14 14:13:58 -0300

    Techinical Logging

--- includes/Technical.php
+++ includes/Technical.php
@@ -10,7 +10,7 @@ class Technical extends Derivative {
     $this->log->lwrite('Starting processing', 'PROCESS_DATASTREAM', $this->pid, $dsid);
     try {
       $output_file = $this->temp_file . '_TECHMD.xml';
-      $command = "/opt/fits/fits.sh -i $this->temp_file -o $output_file &> /var/log/phpfunctions.log";
+      $command = "/opt/fits/fits.sh -i $this->temp_file -o $output_file &> /var/log/phpfunctions/cmd1.log";
       exec($command, $techmd_output, $return);
       $log_message = "$dsid derivative created using FITS with command - $command || SUCCESS";
       $this->add_derivative($dsid, $label, $output_file, 'text/xml', $log_message);
