4789041cb76005061ceb6b9f1aa039461ac49323
CS482 <CS482@tlrmcknz.com>
2013-04-14 14:11:12 -0300

    PDF Logging

--- includes/Pdf.php
+++ includes/Pdf.php
@@ -12,12 +12,12 @@ class Pdf extends Derivative {
       try {
         $output_file = $this->temp_file . '_Scholar_PDFA.pdf';
         if ($this->mimetype == 'application/pdf') {
-          $command = "gs -dPDFA -dBATCH -dNOPAUSE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=$output_file $this->temp_file";
+          $command = "gs -dPDFA -dBATCH -dNOPAUSE -dUseCIEColor -sProcessColorModel=DeviceCMYK -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=$output_file $this->temp_file &> /var/log/phpfunctions/cmd1.log";
           exec($command, $pdfa_output, $return);
           $log_message = "$dsid derivative created using GhostScript with command - $command || SUCCESS";
         }
         else {
-          $command = "java -jar /opt/jodconverter-core-3.0-beta-4/lib/jodconverter-core-3.0-beta-4.jar $this->temp_file $output_file";
+          $command = "java -jar /opt/jodconverter-core-3.0-beta-4/lib/jodconverter-core-3.0-beta-4.jar $this->temp_file $output_file &> /var/log/phpfunctions/cmd1.log";
           exec($command, $pdfa_output, $return);
           $log_message = "$dsid derivative created using JODConverted and OpenOffice with command - $command || SUCCESS";
         }
