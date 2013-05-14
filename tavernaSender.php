b9b189881a9ef001a38b459ad26d7235071dea3a
yqjiang <yqjiang0830@gmail.com>
2013-05-13 23:55:20 -0300

    using tuque

--- tavernaSender.php
+++ tavernaSender.php
@@ -1,15 +1,25 @@
 <?php
       require_once 'sender.php';
       require_once 'TavernaException.php';
+      require_once 'tuque/HttpConnection.php';
                                   
 class TavernaSender extends Sender
-{
-/*
-* intitiallize the
-*/
+{ 
+
+
   function __construct($protocol,$hostname=null,$port=null,$context, $username=null,$password =null)
   {
     parent::__construct($protocol.'://'.$hostname.":".$port .'/'. $context."/rest/runs/",$password,$username);
+  }
+
+
+
+
+  function set_ssl()
+  {
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+          curl_setopt($ch, CURLOPT_SSLVERSION, 3);
   }
   
   //would like to update this function to use tuque HttpConnection class
@@ -17,13 +27,17 @@ class TavernaSender extends Sender
   {
       if (!empty($message))
       {
-          $ch = curl_init();
+ /**         $ch = curl_init();
           curl_setopt($ch, CURLOPT_URL, $this->hostname);
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
           curl_setopt($ch, CURLOPT_POST, true);
+
+	  $this->set_ssl();
+
           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
           curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+
           curl_setopt($ch, CURLOPT_HEADER, true);
           curl_setopt($ch, CURLOPT_USERPWD, "$this->username:$this->password");
           curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/vnd.taverna.t2flow+xml'));
@@ -42,6 +56,26 @@ class TavernaSender extends Sender
           
           curl_close($ch);
           return $content;
+*/
+
+
+         $curl_connect =new CurlConnection();
+	 $curl_connect->username = $this->username;
+	 $curl_connect->password = $this->password;
+
+	 // TO DO: seperate this to an sigal function. 
+	 $curl_connect->sslVersion = 3;
+	 $curl_connect->verifyHost = FALSE;
+	 $curl_connect->verifyPeer = FALSE; 
+
+	 $response = $curl_connect->postRequest($this->hostname,'string',$message,'Content-Type: application/vnd.taverna.t2flow+xml');
+        
+	 if($response['status'] !=201)
+         { 
+		throw new TavernaException($response['headers'].$response['content']);
+	 }
+
+	 return $response['headers'].$response['content'];
       }
       
       return null;
@@ -58,12 +92,13 @@ class TavernaSender extends Sender
     
     return null;
   }
-  
+
+  //HttpConnection:putRequest  
   function run_t2flow($uuid)
   {
       if (!empty($uuid))
       {
-          $ch = curl_init();
+/**          $ch = curl_init();
           curl_setopt($ch, CURLOPT_URL, $this->hostname.$uuid."/status/");
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);    
           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
@@ -85,15 +120,40 @@ class TavernaSender extends Sender
       
       curl_close($ch);
       return $result;
+*/
+
+        $url= $this->hostname.$uuid.'/status/';
+	$curl_connect =new CurlConnection();
+	$curl_connect->username = $this->username;
+	$curl_connect->password = $this->password;
+
+	// TO DO: seperate this to an sigal function. 
+	$curl_connect->sslVersion = 3;
+	$curl_connect->verifyHost = FALSE;
+	$curl_connect->verifyPeer = FALSE; 
+
+	$response = $curl_connect->putRequest($this->hostname,'string','Operating');
+
+	if($response['status'] != 200)
+        { 
+		throw new TavernaException($response['headers'].$response['content']);
+	}
+
+	
+	return $response['headers'].$response['content'];
+ 
       }
       return null;
-  }
+ }
   
+
+  //HttpConnection:getRequest
   function get_status($uuid)
   {
       if (!empty($uuid))
       {
-          $ch = curl_init();
+
+/**        $ch = curl_init();
           curl_setopt($ch, CURLOPT_URL, $this->hostname.$uuid."/status/");
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);                   
           curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
@@ -111,9 +171,29 @@ class TavernaSender extends Sender
               curl_close($ch);
               throw new TavernaException($result);
           }
-      
           curl_close($ch);
+*/
+	 $url=$this->hostname.$uuid."/status/";
+
+         $curl_connect =new CurlConnection();
+	 $curl_connect->username = $this->username;
+	 $curl_connect->password = $this->password;
+
+	 // TO DO: seperate this to an sigal function. 
+	 $curl_connect->sslVersion = 3;
+	 $curl_connect->verifyHost = FALSE;
+	 $curl_connect->verifyPeer = FALSE; 
+
+	 $response = $curl_connect->getRequest($url,'string',$message,$input);
+        
+	 if($response['status'] !=200)
+         { 
+		throw new TavernaException($response['headers'].$response['content']);
+	 }
+	
+	 return $response['headers'].$response['content'];
       }
+
       return null;
   }
   
@@ -121,7 +201,8 @@ class TavernaSender extends Sender
   {
     if (!empty($uuid))
     {
-        $ch = curl_init();
+
+/**      $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->hostname.'/'.$uuid.'/');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);                   
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
@@ -133,7 +214,7 @@ class TavernaSender extends Sender
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('text/plain'));
         $result = curl_exec($ch);
     
-        if (curl_getinfo($ch,CURLINFO_HTTP_CODE) != 202)
+        if ($curl_getinfo($ch,CURLINFO_HTTP_CODE) != 202)
         {
             curl_close($ch);
             throw new TavernaException($result);
@@ -141,19 +222,42 @@ class TavernaSender extends Sender
     
         curl_close($ch);
         return $result;
+*/
+
+	$url= $this->hostname.$uuid.'/';
+
+        $curl_connect =new CurlConnection();
+	$curl_connect->username = $this->username;
+	$curl_connect->password = $this->password;
+
+	// TO DO: seperate this to an sigal function. 
+	$curl_connect->sslVersion = 3;
+	$curl_connect->verifyHost = FALSE;
+	$curl_connect->verifyPeer = FALSE; 
+
+	 $response = $curl_connect->deleteRequest($url);
+        
+	 if($response['status'] !=202)
+         { 
+		throw new TavernaException($response['headers'].$response['content']);
+	 }
+ 	
+	 return $response['headers'].$response['content'];
     }
     
     return null;
-  }
+  } 
+
   //would like to use tuque here
   function add_input($uuid, $key, $value)
   {
       if (!empty($uuid) && !empty($key) && !empty($value))
       {
+
           $input = '<t2sr:runInput xmlns:t2sr="http://ns.taverna.org.uk/2010/xml/server/rest/">
                       <t2sr:value>'.$value.'</t2sr:value>             
                     </t2sr:runInput>';
-          $ch = curl_init();
+/*          $ch = curl_init();
           curl_setopt($ch, CURLOPT_URL, $this->hostname.$uuid."/input/input/".$key);
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);                   
           curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
@@ -175,6 +279,26 @@ class TavernaSender extends Sender
           
           curl_close($ch);
           return $result;
+*/
+	 $url=$this->hostname.$uuid."/input/input/".$key;
+
+         $curl_connect =new CurlConnection();
+	 $curl_connect->username = $this->username;
+	 $curl_connect->password = $this->password;
+
+	 // TO DO: seperate this to an sigal function. 
+	 $curl_connect->sslVersion = 3;
+	 $curl_connect->verifyHost = FALSE;
+	 $curl_connect->verifyPeer = FALSE; 
+
+	 $response = $curl_connect->putRequest($url,'string',$input);
+        
+	 if($response['status'] !=200)
+         { 
+		throw new TavernaException($response['headers'].$response['content']);
+	 }
+
+	 return $response['headers'].$response['content'];
       }
       
       return null;
