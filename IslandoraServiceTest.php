8540c5d57851dd3f3c567d8601d308deebf93f2c
yqjiang <yqjiang0830@gmail.co>
2013-05-21 12:06:46 -0300

    test sucess except read and write

--- IslandoraServiceTest.php
+++ IslandoraServiceTest.php
@@ -6,18 +6,67 @@
  */
 
 require_once 'soap_serv.php';
+
+
 class IslandoraServiceTest extends PHPUnit_Framework_TestCase {
-  
+
   protected $islandoraServ;
-  public function setUp()
-  {
+
+  public function setUp() {
     $this->islandoraServ = new IslandoraService();
+    $this->islandoraServ->IslandoraService();
+  }
+
+  /**
+   * @depends allOcr
+   */
+  public function testAllOcr() {
+    $result = $this->islandoraServ->allOcr('islandora:377', 'JPG', 'ALL_OCR_TEST', 'allOcr function test', 'eng');
+    $this->assertEquals(0,$result);
+  }
+
+  /**
+   * @depends ocr
+   */
+  public function testOcr() {
+    $result = $this->islandoraServ->ocr('islandora:377', 'JPG', 'OCR_TEST', 'ocr function test', 'eng');
+    $this->assertEquals(0,$result);
+  }
+
+  /**
+   * @depends HOCR
+   */
+  public function testHOcr() {
+    $result = $this->islandoraServ->hOcr('islandora:377', '$dsid', 'JPG', 'HOCR_TEST', 'hOcr function test', 'eng');
+    $this->assertEquals(0,$result);
+  }
+
+  /**
+   * @depends encodedOcr
+   */
+  public function testEncodedOcr() {
+    $result = $this->islandoraServ->encodedOcr('islandora:377', '$dsid', 'JPG', 'ENCODED_OCR_TEST', 'encodedOcr function test', 'eng');
+    $this->assertEquals(0,$result);
+  }
+
+  /**
+   * @depends scholarPdfa
+   */
+  public function testScholarPdfa() {
+    $result = $this->islandoraServ->scholarPdfa('islandora:313', 'JPG', 'SCHOLAR_PDFA_TEST', 'scholar pdfa test');
+    $this->assertEquals(0,$result);
   }
   
-  public function testAllOcr()
+  /**
+   * @depends tn
+   */
+  public function testTn()
   {
-    $result = $this->islandoraServ->allOcr('islandora:377', 'JPG', 'NEW_JPG', 'allOcr function test', 'eng');
-    $this->assertEquals(0);
+    $result = $this->islandoraServ->tn('islandora:313', 'JPG', 'TN_TEST', 'tn function test', 400, 400);
+    $this->assertEquals(0,$result);
   }
+  
+
 }
+
 ?>
