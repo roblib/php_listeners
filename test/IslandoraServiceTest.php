e389762f3b9c1cde3e33a4b310526d6e2d2e0494
yqjiang <yqjiang0830@gmail.com>
2013-05-31 14:31:13 -0300

    add taverna sender unit test

--- test/IslandoraServiceTest.php
+++ test/IslandoraServiceTest.php
@@ -21,7 +21,7 @@ class IslandoraServiceTest extends PHPUnit_Framework_TestCase {
   }
 
   function testWrite() {
-    $result = $this->islandoraServ->write('islandora:313', 'STANLEY_JPG', 'tiny Stanley', base64_encode(file_get_contents("soap_test/stanly.jpg")), "image/jpeg");
+    $result = $this->islandoraServ->write('islandora:313', 'STANLEY_JPG', 'tiny Stanley', base64_encode(file_get_contents("test/TestData/stanley.jpg")), "image/jpeg");
     $this->assertEquals($result, 'success');
   }
 
